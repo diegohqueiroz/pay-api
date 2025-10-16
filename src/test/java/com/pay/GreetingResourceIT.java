1200ee67723402a226951c0531986becb775e785
diegohqueiroz <diegohqueiroz@gmail.com>
2025-10-16 15:07:29 -0300

    Estrutura basica

---
+++ src/test/java/com/pay/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.pay;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
